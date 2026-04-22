5efb8ba3461f0057d8895d1a1d8790d540415925
xayam <xayam@yandex.ru>
2026-04-22 11:53:52 +0300

    v2.7.4

--- src/ria/riaconfig.py
+++ src/ria/riaconfig.py
@@ -1,11 +1,16 @@
 
-# for riacalculate.py
+# common
 version = "v2.7"
 site = "ria.ru"
 name = "TrendNews"
 preffix = f"{name}-{version}-{site}-"
 domain = f"https://{site}/"
 
+# for riadownload.py
+count_day = 366 * 3 + 31  # last count days, which will download
+domain_download = f"{domain}sitemap_article.xml?"
+
+# for riacalculate.py
 years = [str(a) for a in range(2024, 2026)]
 months = [str(b).rjust(2, '0') for b in range(12, 0, -1)]
 days = [str(c).rjust(2, '0') for c in range(31, 0, -1)]
@@ -15,7 +20,3 @@ all_months = ["январь", "февраль", "март", "апрель", "м�
               "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
 all_days = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]
 all_stop = ["россия", "сми", "ростёха", "риа", "москва", "новость",  "дек"]
-
-# for riadownload.py
-count_day = 366 * 3 + 31  # last count days, which will download
-domain_download = f"{domain}sitemap_article.xml?"
